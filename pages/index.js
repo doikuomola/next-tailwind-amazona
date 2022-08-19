7b515b379a2753851a869d2d29095a06a7178640
doikuomola <doikuomola@gmail.com>
2022-08-19 13:40:42 +0100

    productitems, static images, @apply tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-import { Layout } from '../components';
+import { Layout, ProductItem } from '../components';
+import data from '../utils/data';
 
 export default function Home() {
-  return <Layout title="homepage">Home page</Layout>;
+  return (
+    <Layout title="homepage">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
